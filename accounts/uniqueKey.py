c48cc13c6e33f65993f9135387339953b71dd295
somerongit <someron.bakuli05@gmail.com>
2020-09-19 21:41:45 +0530

    Someron : Tour add done, basetemplate edit for testing

--- accounts/uniqueKey.py
+++ accounts/uniqueKey.py
@@ -6,19 +6,25 @@ from .models import AccountType
 # Genereting unique Id for travellers -------------
 def travellerId():
     charList='0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
-    uniqueId="k"
+    uniqueId="e"
     for r in range(5):
         uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(userId=uniqueId).exists():
+        uniqueId="k"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(userId=uniqueId).exists():
+        uniqueId="s"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(userId=uniqueId).exists():
+        uniqueId="r"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]            
-    return "".join(['TRAVMAKSUSER',uniqueId])
+    return "".join(['TMUSR',uniqueId])
 # Genereting unique Id for travellers end here -------------
 
 # Genereting unique Id for seller agency -------------
@@ -27,32 +33,44 @@ def sellerId():
     uniqueId="k"
     for r in range(5):
         uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(agentId=uniqueId).exists():
+        uniqueId="i"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(agentId=uniqueId).exists():
+        uniqueId="y"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(agentId=uniqueId).exists():
+        uniqueId="w"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]            
-    return "".join(['TRAVMAKSSELL',uniqueId])
+    return "".join(['TMAGE',uniqueId])
 # Genereting unique Id for seller agency end here -------------
 
 # Genereting unique Id for guide -------------
 def guidId():
     charList='0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
-    uniqueId="k"
+    uniqueId="z"
     for r in range(5):
         uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(guideId=uniqueId).exists():
+        uniqueId="l"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(guideId=uniqueId).exists():
+        uniqueId="h"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
     if AccountType.objects.filter(guideId=uniqueId).exists():
+        uniqueId="f"
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]            
-    return "".join(['TRAVMAKSGUID',uniqueId])
+    return "".join(['TMGUI',uniqueId])
 # Genereting unique Id for guide end here -------------
--- travelagency/tests.py
+++ travelagency/tests.py
@@ -1,11 +1,70 @@
 from django.test import TestCase
 from accounts.models import *
 from .models import *
+import random,math,ast
+from datetime import date
 # Create your tests here.
 def tourIdMaker():
-    tourId = ''
-    return tourId
+    
+    charList='0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+    uniqueId="x"
+    for r in range(5):
+        uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
+    if Tour.objects.filter(tourId=uniqueId).exists():
+        uniqueId="k"
+        for r in range(5):
+            uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
+    if Tour.objects.filter(tourId=uniqueId).exists():
+        uniqueId="p"
+        for r in range(5):
+            uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]
+    
+    if Tour.objects.filter(tourId=uniqueId).exists():
+        uniqueId="b"
+        for r in range(5):
+            uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]            
+    return "".join(['TRAVMAKSTOUR',uniqueId])
+
+#2000-12-26
+def tourDate(sdate):
+    m,d,y=sdate.split('/')
+    fDate = y+'-'+m+'-'+d
+    return fDate
+
+
+def tourDuration(sdate,edate):
+    m,d,y=edate.split('/')
+    y=int(y)
+    m=int(m)
+    d=int(d)
+    d1=date(y,m,d)
+    m,d,y=sdate.split('/')
+    y=int(y)
+    m=int(m)
+    d=int(d)
+    d0=date(y,m,d)
+
+    duration = d1-d0
+    return duration.days
+
 
 def descriptionMaker(description_dct):
-    description = ''
+    description = 'TRAVMAKS'
+    description = description + str(description_dct)
+    return description+'TRAVMAKS'
+
+def descriptionExtractor(description_str):
+    description_str = description_str.strip('TRAVMAKS')
+    description = ast.literal_eval(description_str)
     return description
+
+def testDec(dictionarywww):
+    print("Actual dic : \n",type(dictionarywww),"\n",dictionarywww)
+    x=descriptionMaker(dictionarywww)
+    print("\n\nDic to String :\n",type(x),"\n",x)
+    y=descriptionExtractor(x)
+    print("\n\nString to Dic :\n",type(y),"\n",y)
+
+
