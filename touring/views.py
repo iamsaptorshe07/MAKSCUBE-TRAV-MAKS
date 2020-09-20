0e69efbbf1245a485b3f1cca4411fd1df54fa373
Saptorshe Das <iamsaptorshe07@gmail.com>
2020-09-20 17:03:58 +0530

    Search functionality added, needs to structure the code at front end

--- touring/views.py
+++ touring/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render, redirect
 from django.http import *
 from .models import *
 from accounts.models import  *
+from travelagency.models import *
 from.tests import *
 from django.contrib import messages
 # Create your views here.
 
 def searchTour(request):
+    slocation = request.GET.get('sLocation')
+    elocation = request.GET.get('eLocation')
+    sDate = request.GET.get('sDate')
+    tours = Tour.objects.filter(startingLocation__icontains=slocation,endLocation__icontains=elocation,startDate=sDate)
+    context = {
+        'Tours':tours,
+    }
     return render(request,'touring/tour_search.html')
+
+def advancedSearching(request):
+    pass
 
 def tourDetails(request):
     return render(request,'touring/tour_details.html')
+
