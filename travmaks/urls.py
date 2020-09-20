a0500b49bdf15e82dd6fd5b962f1a6bee94ff7d0
gobinda812 <gobindag951@gmail.com>
2020-09-20 22:50:06 +0530

    gobinda fixed button complete

--- travmaks/urls.py
+++ travmaks/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
 
-admin.site.site_header = "TRAVMAKS ADMIN PANEL - A MAKS CUB3 PRODUCT"
-admin.site.index_title = "TRAVMAKS - We make your travel easy!"
+#admin.site.site_header = "TRAVMAKS ADMIN PANEL - A MAKS CUB3 PRODUCT"
+#admin.site.index_title = "TRAVMAKS - We make your travel easy!"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('',include('homeApp.urls'),name='homeapp'),
     path('accounts/',include('accounts.urls'),name='accounts'),
     path('qna/',include('qna.urls'),name='qnaPage'),
