3eed7c22dde551a70e530fd231a929f442934028
kuntaldinda <kuntaldinda@gmail.com>
2020-09-22 13:35:30 +0530

    Update on home page

--- homeApp/views.py
+++ homeApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import *
+from travelagency.models import *
 
 # Homepage Function
 def index(request):
-    return render(request,'home.html')
+
+    tour = Tour.objects.all()
+    context = {'Tour' : tour}
+    return render(request,'home.html', context=context)
