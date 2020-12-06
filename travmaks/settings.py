b0a7c510106bc9756b7630ddc92b29edf6fd5d52
Saptorshe Das <iamsaptorshe07@gmail.com>
2020-12-06 22:26:26 +0530

    Email backend change

--- travmaks/settings.py
+++ travmaks/settings.py
@@ -166,6 +166,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'designs')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+'''
 DEFAULT_FROM_EMAIL = 'travmaks-admin@travmaks.in'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # Example for using Zoho Mail as email sending backend
@@ -175,3 +176,11 @@ EMAIL_HOST = 'smtp.zoho.in'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'travmaks-admin@travmaks.in'
 EMAIL_HOST_PASSWORD = 'iamtravmaks07!'
+'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+MAILER_EMAIL_BACKEND = EMAIL_BACKEND
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'makscub3e@gmail.com'
+EMAIL_HOST_PASSWORD = 'Weareteammaks07!'
