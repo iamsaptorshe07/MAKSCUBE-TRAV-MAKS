5e5d285e3db1005bbc62349cda1e4a0f1e49bfb7
somerongit <someron.bakuli05@gmail.com>
2020-12-12 21:12:46 +0530

    Someron : Tour Details update!

--- touring/tests.py
+++ touring/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .models import *
 import random,math,ast
+from datetime import datetime
+from datetime import date
 # Create your tests here.
 def OrderIdGenerator():
     
@@ -24,3 +26,13 @@ def OrderIdGenerator():
         for r in range(5):
             uniqueId = uniqueId + charList[math.floor(random.random()*len(charList))]            
     return "".join(['TRAVMAKSORDER',uniqueId])
+
+def canBook(lastDateOfBooking):
+    print("\n\n\n",lastDateOfBooking,"\n\n\n")
+    # d0=datetime.strptime(lastDateOfBooking, "%Y-%m-%d")
+    # print("\n\n\n",d0,"\n\n\n")
+    d1=date.today()
+    if d1<lastDateOfBooking:
+        return True
+    else:
+        return False 
