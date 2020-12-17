7d39322289c50ebf31a158e18c407ca37aad1099
Saptorshe Das <iamsaptorshe07@gmail.com>
2020-12-17 12:12:46 +0530

    accept decline done

--- api/travelagencyAPI/urls.py
+++ api/travelagencyAPI/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
         path('my-agency-tours',MyAgencyTour.as_view(),name='MyAgencyTour'),
         path('my-agency-tour-detail/<tourId>',TourDetail.as_view(),name='TourGetEditDelete'),
         path('ongoing-tour',OngoingTour.as_view(),name='OngoingTour'),
+        path('accept-decline-tour/<orderId>',AcceptOrDeclineTour.as_view(),name='AcceptOrDecline'),
     ]
