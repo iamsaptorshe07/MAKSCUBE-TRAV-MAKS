077545cf3f1fced227953c3fc6a7c0a3533d7c8b
somerongit <someron.bakuli05@gmail.com>
2020-12-18 11:47:10 +0530

    So.. : Preview changed!

--- touring/urls.py
+++ touring/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('preview/<str:tourId>',preview,name='preview'),
     path('search',searchTour,name='searchTour'),
     path('all-tours',AllToursView.as_view(),name='allTours'),
     path('tourdetails/<str:tourId>/<str:slug>',tourDetails,name='tourDetails'),
