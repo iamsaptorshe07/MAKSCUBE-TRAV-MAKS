20311c668b51e72c32147d13661dc6f5112c802d
Saptorshe Das <54545530+iamsaptorshe07@users.noreply.github.com>
2021-07-19 22:37:23 +0530

    Update settings.py

--- travmaks/settings.py
+++ travmaks/settings.py
@@ -164,22 +164,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'designs')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-# TRAVMAKS Production mail 
-'''
-DEFAULT_FROM_EMAIL = 'travmaks-admin@travmaks.in'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# Example for using Zoho Mail as email sending backend
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST = 'smtp.zoho.in'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'travmaks-admin@travmaks.in'
-EMAIL_HOST_PASSWORD = 'iamtravmaks07!'
-'''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 MAILER_EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'makscub3e@gmail.com'
-EMAIL_HOST_PASSWORD = 'Weareteammaks07!'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
